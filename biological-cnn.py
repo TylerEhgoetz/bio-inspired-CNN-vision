e09e3c6e547ba96346d61d4229ca85acea91b060
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-04-07 18:01:47 -0400

    initial cnn setup

--- biological-cnn.py
+++ biological-cnn.py
@@ -1,3 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+MNIST_FASHIONMNIST_CLASSES = 10
+
+
+class SimpleCNN(nn.Module):
+    def __init__(
+        self,
+        use_lateral_inhibition=False,
+        inhibition_strength=0.0,
+        use_non_grid=False,
+        plasticity=False,
+    ):
+        super(SimpleCNN, self).__init__()
+        self.use_lateral_inhibition = use_lateral_inhibition
+        self.placticity = plasticity
+        self.initial_inhibition_strength = inhibition_strength
+        self.current_inhibition_strength = inhibition_strength
+
+        if use_non_grid:
+            # Define non-grid connectivity layers here
+            pass
+        else:
+            self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+
+        if use_lateral_inhibition:
+            # Define lateral inhibition layers here
+            pass
+        else:
+            self.lateral1 = nn.Identity()
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        if use_non_grid:
+            # Define non-grid connectivity layers here
+            pass
+        else:
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+
+        if use_lateral_inhibition:
+            # Define lateral inhibition layers here
+            pass
+        else:
+            self.lateral2 = nn.Identity()
+
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, MNIST_FASHIONMNIST_CLASSES)
+
+    def forward(self, x, return_features=False):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.lateral1(x)
+        x = self.pool(x)
+        # Save features
+        feature_map = x.clone()
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.lateral2(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        if return_features:
+            return x, feature_map
+        return x
+
+
 def run_experiment(
     dataset_name: str,
     inhibition_strength: float,
